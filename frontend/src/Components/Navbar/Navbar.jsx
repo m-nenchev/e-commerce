5481f6ff08f8e54a4a0242f4e971fd3f45013729
m-nenchev <milannn28@abv.bg>
2024-11-04 06:42:51 +0200

    I added commponent Navbar,Popular,Item,Hero and Pages - Cart,LoginSignup<Product,ShopCategory

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 
 import './Navbar.css'
 
+import { Link } from 'react-router-dom';
 import logo from '../Assets/logo.png'
 import cart_icon from '../Assets/cart_icon.png'
 import { useState } from 'react'
@@ -15,14 +16,14 @@ const Navbar = ()=> {
                 <p>SHOPPER</p>
             </div>
             <ul className='nav-menu'>
-                <li onClick={()=>{setMenu("shop")}}>Shop{menu==="shop"?<hr/>:<></>}</li>
-                <li onClick={()=>{setMenu("mens")}}>Men{menu==="mens"?<hr/>:<></>}</li>
-                <li onClick={()=>{setMenu("womens")}}>Women{menu==="womens"?<hr/>:<></>}</li>
-                <li onClick={()=>{setMenu("kids")}}>Kids{menu==="kids"?<hr/>:<></>}</li>
+                <li onClick={()=>{setMenu("shop")}}><Link className='linkLi' to='/'>Shop</Link>{menu==="shop"?<hr/>:<></>}</li>
+                <li onClick={()=>{setMenu("mens")}}><Link className='linkLi' to='/mens'>Men</Link>{menu==="mens"?<hr/>:<></>}</li>
+                <li onClick={()=>{setMenu("womens")}}><Link className='linkLi' to='/womens'>Women</Link>{menu==="womens"?<hr/>:<></>}</li>
+                <li onClick={()=>{setMenu("kids")}}><Link className='linkLi'to='/kids'>Kids</Link>{menu==="kids"?<hr/>:<></>}</li>
             </ul>
             <div className="nav-login-cart">
-                <button>Login</button>
-                <img src={cart_icon} alt="" />
+                <Link to='/login'><button>Login</button></Link>
+                <Link to='/cart'><img src={cart_icon} alt="" /></Link>
                 <div className="nav-cart-count">0</div>
             </div>
         </div>

d0c7f60d5d64e8b10e4fae25bfbbe96d87a540ce
m-nenchev <milannn28@abv.bg>
2024-11-05 10:00:18 +0200

    I Created Component: Offers, Context: ShopContext, Pages: Shop, ShopCategory,

---
+++ frontend/src/Pages/Shop.jsx
@@ -0,0 +1,18 @@
+import Hero from '../Components/Hero/Hero'
+import NewCollections from '../Components/NewCollections/NewCollections';
+import Newsletter from '../Components/Newsletter/Newsletter';
+import Offers from '../Components/Offers/Offers';
+import Popular from '../Components/Popular/Popular';
+
+const Shop = () =>{
+    return(
+        <div>
+          <Hero/>
+          <Popular/>
+          <Offers/>
+          <NewCollections/>
+          <Newsletter/>
+        </div>
+    )
+}
+export default Shop;

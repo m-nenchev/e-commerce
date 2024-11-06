2f80f37afd5e57030f6efe22ac4cfa25f8a75061
m-nenchev <milannn28@abv.bg>
2024-11-06 15:04:12 +0200

    I created Components: CartItems and Updated: Navbar, ProductDisplay, ShopContext, Cart

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -4,11 +4,12 @@ import './Navbar.css'
 import { Link } from 'react-router-dom';
 import logo from '../Assets/logo.png'
 import cart_icon from '../Assets/cart_icon.png'
-import { useState } from 'react'
+import { useContext, useState } from 'react'
+import { ShopContext } from '../../Context/ShopContext';
 const Navbar = ()=> {
 
     const[menu,setMenu] = useState("shop")
-
+    const{getTotalCartItems} = useContext(ShopContext)
     return(
         <div className='navbar'>
             <div className="nav-logo">
@@ -24,7 +25,7 @@ const Navbar = ()=> {
             <div className="nav-login-cart">
                 <Link to='/login'><button>Login</button></Link>
                 <Link to='/cart'><img src={cart_icon} alt="" /></Link>
-                <div className="nav-cart-count">0</div>
+                <div className="nav-cart-count">{getTotalCartItems()}</div>
             </div>
         </div>
     )

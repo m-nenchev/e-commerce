9cc6779244a1f52d979418a7f916b9327cf54160
m-nenchev <milannn28@abv.bg>
2024-12-11 17:07:14 +0200

    I added Admin

---
+++ admin/src/Pages/Admin/Admin.jsx
@@ -0,0 +1,11 @@
+import Sidebar from '../../Components/Sidebar/Sidebar';
+import './Admin.css';
+
+const Admin = () =>{
+    return(
+        <div className="admin">
+          <Sidebar/>
+        </div>
+    )
+}
+export default Admin;
